cc123e984917abd7a223a5f3029f61ecb9faf52d
Taichi Akiyama <taichia@andrew.cmu.edu>
2018-04-03 08:15:26 -0400

    fixed it, my amplitude function was overly complicated

--- matlab_test.m
+++ matlab_test.m
@@ -1,18 +1,22 @@
-F_A = 440;
+F_A = 340;
 F_B = 400;
 
 rate = 44100;
 time = 0.1;
 t = 0:1/rate:time;
 %Assume 100 is the max amplitude
-%a = 100*sin(2*pi*F_A*t);
+%a = 15*sin(2*pi*F_A*t);
 %out = [zeros(size(t)); a]';
+
+%Works fine
 amps = {};
 for i=47:1:86
     aR = (47 - i)*(47-i);
     aL = (39 - i)*(39 - i);
-    ampR = 100*(1 - aL/2209);
-    ampL = 100*(1 - aR/2209);
+    %ampR = 15*(1 - aL/2209);
+    %ampL = 15*(1 - aR/2209);
+    ampR = 15/aL;
+    ampL = 15/aR;
     amps{i-46} =  [ampR, ampL];
 end
 for i=1:1:40
@@ -22,4 +26,5 @@ for i=1:1:40
     sound(out, rate);
     pause(0.1)
 end
-%sound(out, rate)
+
+sound(out, rate)
