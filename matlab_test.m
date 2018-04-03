b659d93c829239d71b06e898283af90e6a824f55
Taichi Akiyama <taichia@andrew.cmu.edu>
2018-04-03 15:33:02 -0400

    trying to do surround

--- matlab_test.m
+++ matlab_test.m
@@ -10,6 +10,7 @@ t = 0:1/rate:time;
 
 %Works fine
 amps = {};
+%for i=47:1:86
 for i=47:1:86
     aR = (47 - i)*(47-i);
     aL = (39 - i)*(39 - i);
@@ -20,11 +21,25 @@ for i=47:1:86
     amps{i-46} =  [ampR, ampL];
 end
 for i=1:1:40
-    left = amps{i}(1)*sin(2*pi*F_A*t);
-    right = amps{i}(2)*sin(2*pi*F_A*t);
+    left = amps{41-i}(1)*sin(2*pi*F_A*t);
+    right = amps{41-i}(2)*sin(2*pi*F_A*t);
     out = [left; right]';
     sound(out, rate);
     pause(0.1)
 end
 
 sound(out, rate)
+%Pause 2 seconds for next noise sample
+
+%pause(2)
+
+xCenter = 43;
+yCenter = 14.5;
+xRadius = 43;
+yRadius = 14.5;
+theta = 0 : 0.01 : 2*pi;
+x = xRadius * cos(theta) + xCenter;
+y = yRadius * sin(theta) + yCenter;
+disp(x)
+around_the_world = {};
+
